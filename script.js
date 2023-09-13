daeac50a9353fbb2d517770078ae9997b64ee3a2
eccos <eccos@users.noreply.github.com>
2023-09-13 11:14:50 -0700

    move globals to runGame(). refactor funcs

--- script.js
+++ script.js
@@ -30,44 +30,44 @@ let testTwo = shuffle(array);
 console.log(testTwo);
 
 // branch 3: add logic to display grid (prompt()) and select card (maybe x,y coord?)
-const UNIQUE_CARDS = [1, 2, 3];
-const PAIR_CARDS = UNIQUE_CARDS.concat(UNIQUE_CARDS);
-const SHUFFLED_CARDS = shuffle(PAIR_CARDS);
-console.log(Object.keys({ SHUFFLED_CARDS })[0], SHUFFLED_CARDS);
-
-const INDEX_LABELS = [];
-for (let i = 0; i < SHUFFLED_CARDS.length; i++) {
-  INDEX_LABELS.push(i);
-}
-
 runGame();
 
 function runGame() {
+  const uniqueCards = [1, 2, 3];
+  const pairCards = uniqueCards.concat(uniqueCards);
+  const shuffledCards = shuffle(pairCards);
+  console.log(Object.keys({ shuffledCards })[0], shuffledCards);
+
+  const cardIndexLabels = [];
+  for (let i = 0; i < shuffledCards.length; i++) {
+    cardIndexLabels.push(i);
+  }
+
   let isWin = false;
   while (!isWin) {
-    const card1Index = cardSelection();
+    const card1Index = cardSelection(shuffledCards, cardIndexLabels);
     if (card1Index === null) { break; }
-    const card1 = SHUFFLED_CARDS[card1Index];
+    const card1 = shuffledCards[card1Index];
 
-    const card2Index = cardSelection();
+    const card2Index = cardSelection(shuffledCards, cardIndexLabels);
     if (card1Index === null) { break; }
-    const card2 = SHUFFLED_CARDS[card2Index];
+    const card2 = shuffledCards[card2Index];
 
     if (card1Index !== card2Index && card1 === card2) {
-      SHUFFLED_CARDS[card1Index] = null;
-      SHUFFLED_CARDS[card2Index] = null;
+      shuffledCards[card1Index] = null;
+      shuffledCards[card2Index] = null;
     }
 
-    isWin = checkWinCondition();
+    isWin = checkWinCondition(shuffledCards);
   }
 }
 
-function cardSelection() {
-  return prompt(`Cards: ${SHUFFLED_CARDS} \nIndex: ${INDEX_LABELS} \nEnter index of card to select`);
+function cardSelection(cards, cardIndexLabels) {
+  return prompt(`Cards: ${cards} \nIndex: ${cardIndexLabels} \nEnter index of card to select`);
 }
 
-function checkWinCondition() {
-  for (card of SHUFFLED_CARDS) {
+function checkWinCondition(cards) {
+  for (const card of cards) {
     if (card !== null) {
       return false;
     }
