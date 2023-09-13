12bc5e30b8a1b0fcde38e429c73ebf1ceef91df4
eccos <eccos@users.noreply.github.com>
2023-09-13 11:02:38 -0700

    refactor global var names again for clarity

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ let testTwo = shuffle(array);
 console.log(testTwo);
 
 // branch 3: add logic to display grid (prompt()) and select card (maybe x,y coord?)
-const UNIQUE_NUMS = [1, 2, 3];
-const PAIR_NUMS = UNIQUE_NUMS.concat(UNIQUE_NUMS);
-const SHUFFLED_PAIRS = shuffle(PAIR_NUMS);
-console.log("shuffledPairs: " + SHUFFLED_PAIRS);
+const UNIQUE_CARDS = [1, 2, 3];
+const PAIR_CARDS = UNIQUE_CARDS.concat(UNIQUE_CARDS);
+const SHUFFLED_CARDS = shuffle(PAIR_CARDS);
+console.log(Object.keys({ SHUFFLED_CARDS })[0], SHUFFLED_CARDS);
 
 const INDEX_LABELS = [];
-for (let i = 0; i < SHUFFLED_PAIRS.length; i++) {
+for (let i = 0; i < SHUFFLED_CARDS.length; i++) {
   INDEX_LABELS.push(i);
 }
 
@@ -47,15 +47,15 @@ function runGame() {
   while (!isWin) {
     const card1Index = cardSelection();
     if (card1Index === null) { break; }
-    const card1 = SHUFFLED_PAIRS[card1Index];
+    const card1 = SHUFFLED_CARDS[card1Index];
 
     const card2Index = cardSelection();
     if (card1Index === null) { break; }
-    const card2 = SHUFFLED_PAIRS[card2Index];
+    const card2 = SHUFFLED_CARDS[card2Index];
 
     if (card1Index !== card2Index && card1 === card2) {
-      SHUFFLED_PAIRS[card1Index] = null;
-      SHUFFLED_PAIRS[card2Index] = null;
+      SHUFFLED_CARDS[card1Index] = null;
+      SHUFFLED_CARDS[card2Index] = null;
     }
 
     isWin = checkWinCondition();
@@ -63,13 +63,12 @@ function runGame() {
 }
 
 function cardSelection() {
-  return prompt(`Cards: ${SHUFFLED_PAIRS} \nIndex: ${INDEX_LABELS} \nEnter index of card to select`);
+  return prompt(`Cards: ${SHUFFLED_CARDS} \nIndex: ${INDEX_LABELS} \nEnter index of card to select`);
 }
 
 function checkWinCondition() {
-  for (let i = 0; i < SHUFFLED_PAIRS.length; i++) {
-    let val = SHUFFLED_PAIRS[i];
-    if (val !== null) {
+  for (card of SHUFFLED_CARDS) {
+    if (card !== null) {
       return false;
     }
   }
